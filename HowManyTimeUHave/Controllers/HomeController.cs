252fc065319d0bd7bfb5ae89adcb6e70e0826821
Sanov96 <sanov1@o2.pl>
2022-01-27 20:36:06 +0100

    Zaciąganie w Controllerze

--- HowManyTimeUHave/Controllers/HomeController.cs
+++ HowManyTimeUHave/Controllers/HomeController.cs
@@ -7,17 +7,41 @@ namespace HowManyTimeUHave.Controllers
 {
     public class HomeController : Controller
     {
+        /*
+        public string mAge { set; get; }
+        public string fAge { set; get; }
+        public string aCnt { set; get; }
+        public string country { set; get; }
+        */
+
         private readonly ILogger<HomeController> _logger;
+
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public string PostUsingParametrs(String mAge, string fAge, string aCnt , string Country)
+        {
+            return "Mother Age: " + mAge + "Father Age" + fAge;
+        }
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
+
+        /*
         public IActionResult Index()
         {
             return View();
         }
+        */
 
         public IActionResult Privacy()
         {
